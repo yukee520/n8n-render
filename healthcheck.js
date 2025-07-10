8e233b2506761e576175d87ee2f10a8d3aaf121f
yukee520 <yukee92tmc@hotmail.com>
2025-07-10 23:19:57 +0800

    Create healthcheck.js

---
+++ healthcheck.js
@@ -0,0 +1,9 @@
+// healthcheck.js
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url === '/health') {
+    res.writeHead(200);
+    res.end('OK');
+  }
+});
+server.listen(8080);
